cc127054f6ece9a3de75314a934836728b180078
Natnipa S <s.natnipa@gmail.com>
2023-07-04 22:08:50 +0200

    create table users

---
+++ prisma/migrations/20230704200744_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('male', 'female');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "gender" "Gender" NOT NULL,
+    "birthdate" TIMESTAMP(3) NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
