59c21d07fb64f48dc3ac618ada9d6511a8639bd8
Natnipa S <s.natnipa@gmail.com>
2023-09-08 17:55:33 +0200

    refactor env keys

--- src/s3/s3Upload.service.ts
+++ src/s3/s3Upload.service.ts
@@ -3,10 +3,10 @@ import * as AWS from 'aws-sdk';
 
 @Injectable()
 export class S3UploadService {
-  AWS_S3_BUCKET = process.env.AWS_S3_BUCKET;
+  AWS_S3_BUCKET = process.env.AWS_S3_IMAGES_BUCKET;
   s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
   });
 
   async uploadImages(files, folder) {
